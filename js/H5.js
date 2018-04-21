93f7f5bb0c5c0d1da73ef2b01f860dff4341e4b8
linshuling <1611074393@qq.com>
2018-04-21 14:39:21 +0800

    内容组织类

--- js/H5.js
+++ js/H5.js
@@ -2,12 +2,13 @@
 * @Author: linshuling
 * @Date:   2018-04-20 14:25:40
 * @Last Modified by:   linshuling
-* @Last Modified time: 2018-04-21 11:57:45
+* @Last Modified time: 2018-04-21 14:26:28
 */
 /*内容管理对象*/
 var H5 = function(){
     this.id = ('h5_' + Math.random()).replace('.','_');
     this.el = $('<div class="h5" id="'+ this.id +'">').hide();
+    this.page = [];
     $('body').append(this.el);
 
     /**
@@ -17,7 +18,7 @@ var H5 = function(){
      * @return {H5} H5对象，可以重复使用H5对象支持的方法
      */
     this.addPage = function(name, text){
-        var page = $('<div class="h5_page">');
+        var page = $('<div class="h5_page section">');
         if(name !== undefined){
             page.addClass('h5_page_' + name);
         }
@@ -25,16 +26,41 @@ var H5 = function(){
             page.text(text);
         }
         this.el.append(page);
+        this.page.push(page);
         return this;
     }
 
     /*新增一个组件*/
-    this.addComponent = function(){
+    this.addComponent = function(name, cfg){
+        var cfg = cfg || {};
+        cfg = $.extend({
+            type : 'base'
+        },cfg);
 
+        var component; //定义一个变量，存储组件元素
+        var page = this.page.slice(-1)[0];
+        console.log(page);
+        switch( cfg.type ){
+            case 'base':
+                component = new H5ComponentBase(name,cfg);
+            break;
+            default:
+        }
+        page.append(component);
+        return this;
     }
 
     /* H5对象初始化呈现 */
     this.loader = function(){
-        
+        this.el.fullpage({
+            /*注意这里的index是从 1 开始的*/
+            onLeave : function(index, nextIndex, direction){
+                $(this).find('.h5_component').eq(index-1).trigger('onLeave');
+            },
+            afterLoad : function(anchorLink, index){
+                $(this).find('.h5_component').eq(index-1).trigger('onLoad');
+            }
+        });
+        this.el.show();
     }
 }
