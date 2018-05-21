f830dd741c76b062d8f234a3e5e2e353a312e823
linshuling <1611074393@qq.com>
2018-05-21 17:28:27 +0800

    第七章-图表组件雷达图开发get

--- js/H5ComponentRadar.js
+++ js/H5ComponentRadar.js
@@ -2,7 +2,7 @@
 * @Author: linshuling
 * @Date:   2018-04-20 14:25:40
 * @Last Modified by:   linshuling
-* @Last Modified time: 2018-05-21 16:57:09
+* @Last Modified time: 2018-05-21 17:24:18
 */
 /*雷达图表组件对象*/
 var H5ComponentRadar = function(name, cfg){
@@ -51,6 +51,30 @@ var H5ComponentRadar = function(name, cfg){
         var y = r + Math.cos(rad) * r; 
         ctx.moveTo(r,r);
         ctx.lineTo(x, y);
+
+        //输出项目文字
+        var text = $('<div class="text">');
+        text.text(cfg.data[i][0]);
+        text.css('transition','all .5s ' + i*.1+'s');
+
+        if( x > w/2){
+            text.css('left', x/2+5);
+        }else{
+            text.css('right', (w-x)/2+5);
+        }
+
+        if( y > h/2){
+            text.css('top', y/2+5);
+        }else{
+            text.css('bottom', (h-y)/2+5);
+        }
+
+        if(cfg.data[i][2]){
+            text.css('color', cfg.data[i][2]);
+        }
+        text.css('opacity',0);
+        component.append(text);
+
     }
     ctx.strokeStyle = '#e0e0e0';
     ctx.stroke();
@@ -63,7 +87,14 @@ var H5ComponentRadar = function(name, cfg){
     component.append(cns);
 
     ctx.strokeStyle = '#f00';
+
     var draw = function(per){ 
+         if(per <= 1 ){
+            component.find('.text').css('opacity',0);
+        }
+        if(per >= 1 ){
+            component.find('.text').css('opacity',1);
+        }
         ctx.clearRect(0,0,w,h);
         //输出数据的折线
         for(var i=0; i<step; i++){
@@ -97,7 +128,7 @@ var H5ComponentRadar = function(name, cfg){
         }
     }
 
-    draw(1);
+    draw(0);
     component.on('onLoad', function(){
         //折线生长动画
         var s = 0;
