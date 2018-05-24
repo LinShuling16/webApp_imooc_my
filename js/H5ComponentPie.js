e470b5a327bc9dfdf90c74261a5fd8bb376a7d7e
linshuling <1611074393@qq.com>
2018-05-24 11:01:08 +0800

    第八章-图表组件饼状图开发get

--- js/H5ComponentPie.js
+++ js/H5ComponentPie.js
@@ -2,46 +2,151 @@
 * @Author: linshuling
 * @Date:   2018-04-20 14:25:40
 * @Last Modified by:   linshuling
-* @Last Modified time: 2018-05-21 17:45:00
+* @Last Modified time: 2018-05-24 10:55:49
 */
-/*雷达图表组件对象*/
+/*饼图表组件对象*/
 var H5ComponentPei = function(name, cfg){
     var component = new H5ComponentBase(name, cfg);
 
     var w = cfg.width;
     var h = cfg.height;
 
-    //加入一个画布 - 雷达图背景
+    //加入一个画布 
     var cns = document.createElement('canvas');
     var ctx = cns.getContext('2d');
     cns.width = ctx.width = w;
     cns.height = ctx.height = h;
+    $(cns).css('zIndex', 1);
     component.append(cns);
 
-    
+    var r = w/2;
 
-    var draw = function(per){ 
-       
+    //绘制一个饼图的底图层
+    ctx.beginPath();
+    ctx.fillStyle = '#eee';
+    ctx.strokeStyle = '#eee';
+    ctx.lineWidth = 1;
+    ctx.arc(r, r, r, 0.2, 2*Math.PI);
+    ctx.fill();
+    ctx.stroke();
+
+    //绘制一个数据层
+    var cns = document.createElement('canvas');
+    var ctx = cns.getContext('2d');
+    cns.width = ctx.width = w;
+    cns.height = ctx.height = h;
+    $(cns).css('zIndex', 2);
+    component.append(cns);
+    
+    var colors = ['drakred','green', 'blue','red', 'orange']; //备用颜色
+    var sAngel = 1.5 * Math.PI; //设置开始的角度在12点位置
+    var eAngel = 0; //结束角度
+    var aAngel = Math.PI * 2; //100%的圆结束角度 2pi = 360
+    
+    var step = cfg.data.length;
+    for(var i=0; i<step; i++){
+
+        var item = cfg.data[i];
+        console.log(item[1]);
+        var color = item[2] || (item[2] =( colors.pop() ) );
+
+        eAngel = sAngel + aAngel * item[1];
+
+        ctx.beginPath();
+        ctx.fillStyle = color;
+        ctx.strokeStyle = color;
+        ctx.lineWidth = .1;
+
+        ctx.moveTo(r,r);
+        ctx.arc(r, r, r, sAngel,eAngel);
+        ctx.fill();
+        ctx.stroke();
+
+        sAngel = eAngel;
+
+        //加入所有项目文本以及百分比
+        var text = $('<div class="text">');
+        text.text(item[0]);
+        var per = $('<div class="per">');
+        per.text( item[1]*100 + '%');
+        text.append(per);
+
+        var x = r + Math.sin( .5 * Math.PI - sAngel) * r;
+        var y = r + Math.cos( .5 * Math.PI - sAngel) * r;
+
+        if( x > w/2 ){
+            text.css('left', x/2);
+        }else{
+            text.css('right', (w-x)/2);
+        }
+
+        if( y > h/2 ){
+            text.css('top', y/2);
+        }else{
+            text.css('bottom', (h-y)/2 +10);
+        }
+
+        if( item[2] ){
+            text.css('color', item[2]);
+        }
+        text.css('opacity',0);
+        component.append(text);
     }
 
+    //加入一个蒙板层
+    var cns = document.createElement('canvas');
+    var ctx = cns.getContext('2d');
+    cns.width = ctx.width = w;
+    cns.height = ctx.height = h;
+    $(cns).css('zIndex', 3);
+    component.append(cns);
+  
+    ctx.fillStyle = '#eee';
+    ctx.strokeStyle = '#eee';
+    ctx.lineWidth = 1;
+    
+    //生长动画
+    var draw = function(per){ 
+        ctx.clearRect(0, 0, w, h);
+
+        ctx.beginPath();
+
+        ctx.moveTo(r,r);
+
+        if(per <=0 ){
+            ctx.arc(r, r, r, 0, 2*Math.PI);
+             component.find('.text').css('opacity', 0);
+        }else{
+            ctx.arc(r, r, r, sAngel, sAngel+2*Math.PI*per,true);
+        }
+        
+        ctx.fill();
+        ctx.stroke();
+
+        if( per >= 1){
+            component.find('.text').css('opacity', 1);
+        }
+
+    }
     draw(0);
+
     component.on('onLoad', function(){
-        //折线生长动画
+        //饼图生长动画
         var s = 0;
         for(var i=0; i<100; i++){
             setTimeout(function(){
                 s+=.01;
-                // draw(s);
+                draw(s);
             },i*10 + 600);
         }
     })
     component.on('onLeave', function(){
-        //折线退场动画
+        //饼图退场动画
         var s = 1;
         for(var i=0; i<100; i++){
             setTimeout(function(){
                 s-=.01;
-                // draw(s);
+                draw(s);
             },i*10);
         }
     })
